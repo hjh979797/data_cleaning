af452742af8fb749ac1a39019e27201a60bf9bec
tom_glb <123glb@qq.com>
2020-12-26 15:20:36 +0800

    12.26 实现传送字段封装

--- data_cleaning/src/components/yumiao/sort.vue
+++ data_cleaning/src/components/yumiao/sort.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div v-for="sel in mylist" :key="sel.id">
-            <el-select v-model="value_tablename" placeholder="请选择">
+            <el-select v-model="sel.value" placeholder="请选择">
                 <el-option
                 v-for="item in tablenames"
                 :key="item.val"
@@ -9,7 +9,7 @@
                 :value="item.val">
                 </el-option>
             </el-select>
-            <el-select v-model="value_sortrule" placeholder="请选择">
+            <el-select v-model="sel.rule" placeholder="请选择">
                 <el-option
                 v-for="item in sortrules"
                 :key="item.val"
@@ -32,7 +32,7 @@ export default{
             value_sortrule:"",
             str:"name,age",
             rules:"升序,降序",
-            mylist:[{id:1}],
+            mylist:[{id:1,value:"", rule:""}],
             value_tablenames:[],
             value_sortrules:[],
         }
@@ -41,23 +41,64 @@ export default{
         this.tablenames=this.str.split(",").map(function(item){
         return{val: item};
       });
-    //   for(var j=0;j<this.tablenames.length;j++)
-    //   {
-    //       console.log(this.tablenames[j].val)
-    //   }
       this.sortrules=this.rules.split(",").map(function(item){
         return{val: item};
       });
     },
     methods:{
         getsort:function(){
-            console.log(this.value_tablenames);
-            console.log(this.value_sortrules);
-            this.$http({
-                url:'/table/tbl-10000/logs',
+        //     console.log(this.value_tablenames);
+        //     console.log(this.value_sortrules);
+        //     this.$http({
+        //         url:'/table/tbl-10000/logs',
+        //         method:"get",
+        //         params:{
+        //             tableName: "tbl-10000",
+        //         },
+        //         headers:{
+        //             Authorization: this.$store.getters.getToken
+        //         }
+        //     }).then(res=>{
+        //         console.log(res);
+        //     },error=>{
+        //         console.log("错误：",error.message)
+        //     })
+            
+            var rules = "";
+            var flag=0;
+             for(var i = 0; i < this.mylist.length;i++)
+             {
+                if(flag==1)
+                {
+                    rules+=",";
+                }
+                flag=1;
+                var tablenametemp=this.mylist[i].value;
+                var ruletemp=this.mylist[i].rule;
+                if(tablenametemp=="")
+                { 
+                    alert("请选择排序字段");
+                    return 0;
+                    
+                }
+                if(ruletemp=="")
+                {
+                    alert("请选择排序规则");
+                    return 0;
+                }
+                else if(ruletemp=="升序")
+                {
+                    ruletemp="desc";    
+                }
+                rules+=tablenametemp+","+ruletemp;
+             }
+             this.$http({
+                url:'/table/tbl-10000/sort',
                 method:"get",
                 params:{
                     tableName: "tbl-10000",
+                    rules: rules,
+                    logId:1000,
                 },
                 headers:{
                     Authorization: this.$store.getters.getToken
@@ -68,9 +109,19 @@ export default{
                 console.log("错误：",error.message)
             })
         },
+        fill:function(){
+        //发送get请求
+            this.$http.get('/table/tableName/fill',this.form).then(function(res){
+                console.log(res);
+            },function(){
+                console.log('请求失败处理');
+            });
+        },
         add:function(){
           var index=this.mylist.length;
-          this.mylist.push({id:index+1});  
+          var val = "";
+          var rul = "";
+          this.mylist.push({id:index+1,value:val,rule:rul});  
         }
     }
 }
